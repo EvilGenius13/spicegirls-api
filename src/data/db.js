8921fdc045905a40599f0fdbdec55cdd04a8378f
Josh Faigan <djjoker13@gmail.com>
2023-05-02 11:11:34 -0400

    switched to atlas cloud

--- src/data/db.js
+++ src/data/db.js
@@ -1,10 +1,29 @@
+// // Requirements
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// // Connect to MongoDB
+// const mongoUri = 'mongodb://localhost/spicegirls';
+
+// mongoose.connect(mongoUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(error => console.error('Error connecting to MongoDB: ', error));
+
+// module.exports = mongoose.connection;
+
 // Requirements
 require('dotenv').config();
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI;
+const ATLAS_USER = process.env.ATLAS_USER;
+const ATLAS_PASSWORD = process.env.ATLAS_PASSWORD;
 
 // Connect to MongoDB
-const mongoUri = 'mongodb://localhost/spicegirls';
+const mongoUri = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@spicegirls-cluster.9aibwdo.mongodb.net/spicegirls`;
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
