49e759f57403d83f9c9e752821ab15e55b8617e7
Josh Faigan <djjoker13@gmail.com>
2023-05-03 18:24:57 -0400

    added deployment instructions for DB

--- src/data/db.js
+++ src/data/db.js
@@ -1,29 +1,11 @@
-// // Requirements
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// // Connect to MongoDB
-// const mongoUri = 'mongodb://localhost/spicegirls';
-
-// mongoose.connect(mongoUri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log('Connected to MongoDB'))
-// .catch(error => console.error('Error connecting to MongoDB: ', error));
-
-// module.exports = mongoose.connection;
-
-// Requirements
 require('dotenv').config();
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI;
 const ATLAS_USER = process.env.ATLAS_USER;
 const ATLAS_PASSWORD = process.env.ATLAS_PASSWORD;
+const ATLAS_URI = process.env.ATLAS_URI;
 
 // Connect to MongoDB
-const mongoUri = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}@spicegirls-cluster.9aibwdo.mongodb.net/spicegirls`;
+const mongoUri = `mongodb+srv://${ATLAS_USER}:${ATLAS_PASSWORD}${ATLAS_URI}`;
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
@@ -33,3 +15,4 @@ mongoose.connect(mongoUri, {
 .catch(error => console.error('Error connecting to MongoDB: ', error));
 
 module.exports = mongoose.connection;
+
